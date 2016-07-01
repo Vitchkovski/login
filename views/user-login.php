dbcf4971e03465d392bdd24c9f0478b28c8bb936
Art <lsa15209@gmail.com>
2016-07-01 23:04:45 +0300

    Third version, new design

--- views/user-login.php
+++ views/user-login.php
@@ -6,55 +6,63 @@
     <title>Registration Page</title>
 
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+   
 </head>
 
 <body class="main">
     <div class="wrap">
         <div class="header">
-            <h1>Registration</h1>
         </div>
-        <div>
+        <div align = "center">
             <form class="vertical-form" method="post">
+                <h1>Registration</h1>
 
-                <input type="email" name="email" placeholder="Email Address" spellcheck="false" value="<?=$email?>" required>
-
-
-
-                <input type="text" name="login" placeholder="Login" value="<?=$login?>" required>
+                <input type="email" name="email" placeholder="Email Address" spellcheck="false" value="<?=$email?>" required><br>
 
 
 
-                <input type="password" name="pass" placeholder="Password" value="" required>
+                <input type="text" name="login" placeholder="Login" value="<?=$login?>" required><br>
+
+
+
+                <input type="password" name="pass" placeholder="Password" value="" required><br>
 
 
                 <input type="submit" value="Save"><br>
-
+            </form>
+        </div>
                 <?php if (!empty($_POST) && $id == 0) { ?>
+        <div align = "center">
+            <form class="vertical-form-bottom">
 
 
                 <input id="error" type="hidden" readonly><label for="error">User you entered is already exist. Either provide a correct password or create a new user.</label>
-
+            </form>
+        </div>
 
                 <?php }
                 
                 if (!empty($_POST) && $id !== 0 && $logged_user_flag == 1) {?>
+        <div align = "center">
+            <form class="vertical-form-bottom">
 
-
-                <input id="info" type="hidden" readonly><label for="info">Welcome, <?=$login?>. Your ID is: <?=$id?></label>
-
+                <input id="info" type="hidden" readonly><label for="info">Welcome, <?=$login?>. Your ID is: <?=$id?></label>
+            </form>
+        </div>
 
 
                 <?php } 
                 if (!empty($_POST) && $id !== 0 && $logged_user_flag == 0) {?>
-
-                <input id="info" type="hidden" readonly><label for="info">User <?=$login?> has been created. User ID is: <?=$id?></label>
-
+        <div align = "center">
+            <form class="vertical-form-bottom">
+                <input id="info" type="hidden" readonly><label for="info">User <?=$login?> has been created. User ID is: <?=$id?></label>
+            </form>
+        </div>
                 <?php } ?>
 
 
-            </form>
-        </div>
+            
+    
     </div>
         <footer>
         Registration Page / 2016
