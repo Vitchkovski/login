f26ce7e727bff5fc3ecbf989fc879e0a7241ed06
Art <lsa15209@gmail.com>
2016-07-16 17:29:24 +0300

    - database.php format changes - root pass updated

--- database.php
+++ database.php
@@ -1,55 +1,50 @@
 <?php
 
-define('MYSQL_SERVER','localhost');
-define('MYSQL_USER','root');
-define('MYSQL_PASSWORD','');
-define('MYSQL_DB','project');
+define('MYSQL_SERVER', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'vbnqp98F');
+define('MYSQL_DB', 'project');
 
 
-
-class DBfunctions 
+class DBfunctions
 {
     private $connectDB = false;
-	private $sqlDataReturn = array();
+    private $sqlDataReturn = array();
 
-	public function __construct() 
+    public function __construct()
     {
-	   $this->connectDB = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
-	   $this->connectDB->connect_error;
-	}
+        $this->connectDB = new mysqli(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB);
+        $this->connectDB->connect_error;
+    }
 
-	public function qrySelect($sql) 
+    public function qrySelect($sql)
     {
         $qry = $this->connectDB->query($sql);
-        
-        if($qry->num_rows > 0) 
-        {
-            while($row = $qry->fetch_assoc()) 
-            {
+
+        if ($qry->num_rows > 0) {
+            while ($row = $qry->fetch_assoc()) {
                 $this->sqlDataReturn["id"] = $row['id'];
                 $this->sqlDataReturn["login"] = $row['login'];
                 $this->sqlDataReturn["email"] = $row['email'];
             }
-        } 
-        else 
-        {
-		  $this->sqlDataReturn["id"] = null;
+        } else {
+            $this->sqlDataReturn["id"] = null;
         }
         return $this->sqlDataReturn;
-		$this->connectDB->close();
+        $this->connectDB->close();
     }
 
-    public function qryFire($sql) 
+    public function qryFire($sql)
     {
-		$this->connectDB->query($sql);
-	}
-            
+        $this->connectDB->query($sql);
+    }
+
     public function escapeString($string)
     {
         $escapedString = $this->connectDB->real_escape_string($string);
         return $escapedString;
     }
-	
+
 }
 
 ?>
