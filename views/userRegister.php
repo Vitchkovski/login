e3cb34e16db8344a351771f1ddc665e8cd5eac67
Art <lsa15209@gmail.com>
2016-07-16 17:18:21 +0300

    userRegister format changes

--- views/userRegister.php
+++ views/userRegister.php
@@ -7,47 +7,50 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="main">
-    <div class="wrap">
-        <div class="header">
-        </div>
-        <div align="center">
-
-            <form class="vertical-form" method="post">
-                <h1>Registration</h1>
-                <input type="email" name="email" placeholder="Email Address" spellcheck="false" value="<?=$userEscapedEmail?>" required><br>
-                <input type="text" name="login" placeholder="Username" value="<?=$userEscapedLogin?>" required><br>
-                <input type="password" name="password" placeholder="Password" value="" required><br>
-                <input type="submit" value="Save"><br>
-                <a class="sign-in" href="login">Already have an account? Login</a>
-                <br>
-            </form>
-        </div>
- 
-        <?php if (!empty($_POST) && isset($userIsAlreadyExistFlag)) 
-        { ?>
-        <div align="center">
-            <form class="vertical-form-bottom">
-                <input id="error" type="hidden" readonly><label for="error">User with such name or email is already exist. Either provide another credentials or <a class="inner-text-href" href="login">log in</a></label>
-            </form>
-        </div>
-
-        <?php 
-        } ?>
-
-        <?php if (isset($passwordIsToShortFlag)) 
-        {?>
-        <div align="center">
-            <form class="vertical-form-bottom">
-                <input id="error" type="hidden" readonly><label for="error">Password should contain at least 6 signs!</label>
-            </form>
-        </div>
-        <?php 
-        } ?>
-
-
+<div class="wrap">
+    <div class="header">
     </div>
-    <footer>
-        Registration Page / 2016
-    </footer>
+    <div align="center">
+
+        <form class="vertical-form" method="post">
+            <h1>Registration</h1>
+            <input type="email" name="email" placeholder="Email Address" spellcheck="false"
+                   value="<?= $userEscapedEmail ?>" required><br>
+            <input type="text" name="login" placeholder="Username" value="<?= $userEscapedLogin ?>" required><br>
+            <input type="password" name="password" placeholder="Password" value="" required><br>
+            <input type="submit" value="Save"><br>
+            <a class="sign-in" href="login">Already have an account? Login</a>
+            <br>
+        </form>
+    </div>
+
+    <?php if (!empty($_POST) && isset($userIsAlreadyExistFlag)) { ?>
+        <div align="center">
+            <form class="vertical-form-bottom">
+                <input id="error" type="hidden" readonly><label for="error">User with such name or email is already
+                    exist. Either provide another credentials or <a class="inner-text-href" href="login">log
+                        in</a></label>
+            </form>
+        </div>
+
+        <?php
+    } ?>
+
+    <?php if (isset($passwordIsToShortFlag)) {
+        ?>
+        <div align="center">
+            <form class="vertical-form-bottom">
+                <input id="error" type="hidden" readonly><label for="error">Password should contain at least 6
+                    signs!</label>
+            </form>
+        </div>
+        <?php
+    } ?>
+
+
+</div>
+<footer>
+    Registration Page / 2016
+</footer>
 </body>
 </html>
