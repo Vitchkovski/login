4a91ef157a2a37af94c98d8f9d55bcdc53026850
Art <lsa15209@gmail.com>
2016-07-16 17:26:09 +0300

    login model format changes

--- login/index.php
+++ login/index.php
@@ -3,102 +3,84 @@
 require_once("../database.php");
 require_once("../models/userFunctions.php");
 
-if (isset($_GET['action']))
 //destroying session in case of log out
-{
+if (isset($_GET['action'])) {
     $action = $_GET['action'];
     if ($action == "logout")
         session_start();
-        session_unset();
-        session_destroy();
+    session_unset();
+    session_destroy();
 }
 
-if (!empty($_POST))
 //Something has been submitted through the form
-{
+if (!empty($_POST)) {
     //escaping special & space characters first for all input
-    if (!empty($_POST['email']))
-    {
-       $userEscapedEmail = htmlspecialchars(ltrim(rtrim($_POST['email'])));
+    if (!empty($_POST['email'])) {
+        $userEscapedEmail = htmlspecialchars(ltrim(rtrim($_POST['email'])));
     }
-    
-    if (!empty($_POST['password']))
-    {
+
+    if (!empty($_POST['password'])) {
         $userEscapedPassword = htmlspecialchars($_POST['password']);
     }
 
     $userEscapedLogin = "";
-    
+
     $userInfo = retriveUserInfo($userEscapedLogin, $userEscapedEmail, $userEscapedPassword);
-    
-    if (isset($userInfo['id']))
+
     //credentials are correct
-    {
-        
+    if (isset($userInfo['id'])) {
+
         (int)$userId = $userInfo['id'];
         $userEmail = $userInfo['email'];
         $userName = $userInfo['login'];
-        
+
         session_start();
         $_SESSION['thisIsLoggedUser'] = true;
-        
+
         $_SESSION['userSessionId'] = $userId;
         $_SESSION['userSessionEmail'] = $userEmail;
         $_SESSION['userSessionName'] = $userName;
-        
+
         include("../views/userPersonalInfo.php");
         //Login Succesfull
-    } 
-    else 
-    {
+    } else {
         //credentials are incorrect. Notifying user
-        
+
         $credentialsAreIncorrectFlag = true;
-        
+
         include("../views/userLogin.php");
     }
-    
-    
-}
-    
-else
-//Nothing submitted yet - form just opened. Defining variables.
-{   
+
+
+} else {
+    //Nothing submitted yet - form just opened. Defining variables.
     session_start();
-    
+
     //Migrating credentials from registration form
-    if (isset($_SESSION['userSessionLogin']))
-    {
+    if (isset($_SESSION['userSessionLogin'])) {
         $userEscapedLogin = $_SESSION['userSessionLogin'];
     }
-    if (isset($_SESSION['userSessionEmail']))
-    {
+    if (isset($_SESSION['userSessionEmail'])) {
         $userEscapedEmail = $_SESSION['userSessionEmail'];
     }
-    
+
     //Redirecting authorized user to the personal info page
-    if (isset($_SESSION['thisIsLoggedUser']))
-    {
+    if (isset($_SESSION['thisIsLoggedUser'])) {
         $userId = $_SESSION['userSessionId'];
         $userEmail = $_SESSION['userSessionEmail'];
         $userName = $_SESSION['userSessionName'];
         include("../views/userPersonalInfo.php");
-    }
-    else
-    //Session is not started for the user - opening login page
-    {
-        
-        if (!isset($userEscapedEmail)) 
-        {
+    } else {
+        //Session is not started for the user - opening login page
+        if (!isset($userEscapedEmail)) {
             $userEscapedEmail = "";
         }
-        if (!isset($userEscapedLogin)) 
-        {
+        if (!isset($userEscapedLogin)) {
             $userEscapedLogin = "";
         }
         include("../views/userLogin.php");
     }
-        
+
 }
 
 ?>
--- models/userFunctions.php
+++ models/userFunctions.php
@@ -1,60 +1,62 @@
 <?php
 
-function addNewUserToDB ($login, $email, $password)
+function addNewUserToDB($login, $email, $password)
 {
     $db_functions = new DBfunctions();
-    
+
     $escapedEmail = $db_functions->escapeString($email);
     $escapedLogin = $db_functions->escapeString($login);
-    
-    $hashedPassword = hash( 'sha512', $password );
-    
+
+    $hashedPassword = hash('sha512', $password);
+
     $queryToRun = sprintf("insert into users (login, email, password) values ('%s', '%s', '%s')", $escapedLogin, $escapedEmail, $hashedPassword);
-    
-	$sqlDataReturn = $db_functions->qryFire($queryToRun);
-    
+
+    $sqlDataReturn = $db_functions->qryFire($queryToRun);
+
     return true;
-    
+
 }
 
-function checkIfUserExist ($login, $email)
+function checkIfUserExist($login, $email)
 //checking if user with provided params is already exist
 {
     $db_functions = new DBfunctions();
-    
+
     $escapedEmail = $db_functions->escapeString($email);
     $escapedLogin = $db_functions->escapeString($login);
-    
+
     $queryToRun = sprintf("select * from users where login = '%s' or email = '%s'", $escapedLogin, $escapedEmail);
-    
+
     $userInfo = $db_functions->qrySelect($queryToRun);
-    
+
     if (!is_null($userInfo['id']))
         return true;
-    
+
     return false;
-    
+
 }
 
-function retriveUserInfo ($login, $email, $password)
+function retriveUserInfo($login, $email, $password)
 {
     $db_functions = new DBfunctions();
-    
+
     $escapedEmail = $db_functions->escapeString($email);
     $escapedLogin = $db_functions->escapeString($login);
-    $hashedPassword = hash( 'sha512', $password );
-    
+    $hashedPassword = hash('sha512', $password);
+
     $queryToRun = sprintf("select * from users where (login = '%s' or email = '%s') and password = '%s'", $escapedLogin, $escapedEmail, $hashedPassword);
-    
+
     $userInfo = $db_functions->qrySelect($queryToRun);
-    
+
     return $userInfo;
 }
 
-function validateIfPasswordSecure($password){
-    if (strlen($password) < 6) 
+function validateIfPasswordSecure($password)
+{
+    if (strlen($password) < 6)
         return false;
-    
+
     return true;
 }
+
 ?>
