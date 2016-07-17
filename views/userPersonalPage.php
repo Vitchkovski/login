ff551ddb464df4180f4884d874b18f1283c5f7cb
Art <lsa15209@gmail.com>
2016-07-17 22:24:34 +0300

    deleted add product button on personal page, made add-form always visible

--- views/userPersonalPage.php
+++ views/userPersonalPage.php
@@ -21,16 +21,9 @@
             <input type="submit" value="Log Out"><br>
         </form>
 
-        <?php if (empty($_POST['newUserProduct'])) { ?>
-        <form action="../myPage/" method="post">
-            <th>
-                <input type="hidden" name="newUserProduct" value="true">
-                <input type="submit" value="Add Product">
-            </th>
-        </form>
-        <?php } ?>
 
-        <?php if (!empty($_POST['newUserProduct'])) { ?>
+
+
             <form enctype="multipart/form-data" class="product-add-form" method="post" action="../myPage/">
                 <input name="productPicture" type="file" >
                 <input type="text" name="productName" placeholder="Product Name" autofocus required>
@@ -38,7 +31,7 @@
                 <input type="hidden" name="newUserProductSubmitted" value="true">
                  <input type="submit" value="Add">
             </form>
-        <?php } ?>
+
 
         <?php if (!is_null($userProducts[0])) { ?>
 
