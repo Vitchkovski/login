4b909a165677c64d0ab26a41092f551fdc3fdd37
Art <lsa15209@gmail.com>
2016-08-24 16:14:37 +0300

    login / email input fileds order changed to prevent incorrect form autofilling by browser password manager

--- application/views/login/userRegister.php
+++ application/views/login/userRegister.php
@@ -7,11 +7,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <form class="form-signin" action="<?php echo base_url("index.php/users/register"); ?>" method="post">
         <h2 class="form-signin-heading">Registration</h2>
         <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
-        <input type="email" class="form-control" name="email" placeholder="Email Address" spellcheck="false"
-               value="<?php echo set_value('email'); ?>" required maxlength="254">
 
         <input type="text" class="form-control" name="login" placeholder="Username"
                value="<?php echo set_value('login'); ?>" required maxlength="254">
+        <input type="email" class="form-control" name="email" placeholder="Email Address" spellcheck="false"
+               value="<?php echo set_value('email'); ?>" required maxlength="254">
 
         <input type="password" class="form-control" name="password" placeholder="Password" value="" required
                maxlength="254">
